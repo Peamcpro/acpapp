562bba9099c135a5ed84729a2fc8c71304a23532
Peamcpro <66110040@kmitl.ac.th>
2024-10-18 10:29:23 +0700

    Create profile.js

---
+++ nextjs/pages/user/profile.js
@@ -0,0 +1,13 @@
+// pages/profile.js
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div>
+      <h1>User Profile</h1>
+      {/* Display user info here */}
+    </div>
+  );
+};
+
+export default Profile;
